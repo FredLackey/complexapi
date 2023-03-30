3952341b5a24b00f5c4c4e79e865fa7bfb6a290d
Fred Lackey <fred.lackey@gmail.com>
2023-03-30 05:33:22 -0400

    imporve shutdown

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:16.20.0-bullseye-slim
+LABEL maintainer="Fred Lackey <fred.lackey@gmail.com>"
+LABEL build-date="2023-03-30T00:15:33-07:00"
+LABEL name="complexapi"
+LABEL version="0.0.1"
+LABEL description="Placeholder API used in POCs and sandbox applications."
+LABEL url="https://github.com/FredLackey/complexapi"
+
+RUN apt-get update && apt-get install -y --no-install-recommends dumb-init
+
+ENV NODE_ENV production
+
+WORKDIR /home/node/app
+
+COPY --chown=node:node . /home/node/app
+
+RUN npm ci --only=production
+
+USER node
+
+EXPOSE 3000
+
+CMD [ "dumb-init", "node", "./src/index.js" ]
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 require('dotenv').config();
 
 const express = require('express');
-const pkg = require('../package.json');
-const routes = require('./routes');
+const pkg     = require('../package.json');
+const routes  = require('./routes');
 
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(routes);
 
-const NODE_ALIAS  = process.env.NODE_ALIAS || "(not set)"
-const NODE_BASE   = process.env.NODE_BASE  || "(not set)"
-const NODE_ENV    = process.env.NODE_ENV || "(not set)"
-const NODE_PORT   = process.env.NODE_PORT || 3000;
+const NODE_ALIAS = process.env.NODE_ALIAS || "(not set)"
+const NODE_BASE  = process.env.NODE_BASE  || "(not set)"
+const NODE_ENV   = process.env.NODE_ENV || "(not set)"
+const NODE_PORT  = process.env.NODE_PORT || 3000;
 
 app.use((err, req, res, next) => {
   console.log(err.stack)
   res.status(500).send('Something broke!')
 })
+
+async function closeGracefully(signal) {
+  console.log(`Received signal to terminate: ${signal}`)
+  process.kill(process.pid, signal);
+}
+process.once('SIGINT', closeGracefully)
+process.once('SIGTERM', closeGracefully)
 
 app.listen(NODE_PORT, (err) => {
   if (err) { throw err; }
